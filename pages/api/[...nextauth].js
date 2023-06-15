ca877dbcd4c4d6942391e2a888a6f09db9327836
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-15 13:50:56 +0100

    Update [...nextauth].js

--- pages/api/[...nextauth].js
+++ pages/api/[...nextauth].js
@@ -1,14 +1,17 @@
 
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+require('dotenv').config();
+
 export const authOptions = {
-  secret: process.env.NextAuth_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
     }),
     // ...add more providers here
   ],
