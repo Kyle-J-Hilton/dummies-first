0228f27bd7f2bd0649e3c619ac64a0a66396bb82
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-16 23:38:52 +0100

    Update HSC.jsx

--- pages/components/HSC.jsx
+++ pages/components/HSC.jsx
@@ -26,7 +26,7 @@ const HSC = () => {
     // Simulate loading completion after 2 seconds (replace this with your own loading logic)
     setTimeout(() => {
       setIsLoading(false);
-    }, 7000);
+    }, 6000);
   }, []);
 
 
