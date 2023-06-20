740c0b3c28f3220644716e7492e4f5a198ee37f3
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-20 10:15:21 +0100

    Update RippleLink.jsx

--- pages/components/RippleLink.jsx
+++ pages/components/RippleLink.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "../../styles/SideScroll.module.css";
+import Link from "next/link";
 
 const RippleLink = () => {
   return (
-    <div className={styles.rippleBox}>
-        <a href="https://twitter.com/DummiesLab" className={styles.shiny}></a>
+    <Link href="https://twitter.com/DummiesLab" className={styles.rippleBox}>
+        <a target="_blank" className={styles.shiny}></a>
         <span style={{animationDelay: "-.3s"}}></span>
         <span style={{animationDelay: "-.6s"}}></span>
         <span style={{animationDelay: "-.9s"}}></span>
         <span style={{animationDelay: "-1.2s"}}></span>
    
-    </div>
+    </Link>
   );
 };
 export default RippleLink;
