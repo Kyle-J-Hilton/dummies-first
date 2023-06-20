1a5fb16428030e53df4a30a8fdd7887fc2a45657
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-20 15:02:28 +0100

    Update Loader.jsx

--- pages/components/Loader.jsx
+++ pages/components/Loader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import AnimatedLogo from "../assets/images/loadinglogo.gif";
+import StillLogo from "../assets/images/loadinglogostill.png";
 import styles from "../../styles/Loader.module.css";
 import Image from "next/image";
 
 function Loader() {
   return (
     <div className={styles.loader}>
-      <Image className={styles.logo} src={AnimatedLogo} alt="loader" />
+      <Image className={styles.logo} src={StillLogo} alt="loader" />
     </div>
   );
 }
