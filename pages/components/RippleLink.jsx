ac950e3da314014a1367bffee9849dd18eb48edd
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-20 10:24:53 +0100

    Update RippleLink.jsx

--- pages/components/RippleLink.jsx
+++ pages/components/RippleLink.jsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 const RippleLink = () => {
   return (
     <div className={styles.rippleBox}>
-        <a target="_blank" href="https://twitter.com/DummiesLab" rel="noopener noreferrer" className={styles.shiny}>
-          <span style={{animationDelay: "-.3s"}}></span>
-          <span style={{animationDelay: "-.6s"}}></span>
-          <span style={{animationDelay: "-.9s"}}></span>
-          <span style={{animationDelay: "-1.2s"}}></span>
-       </a>
+        <a target="_blank" href="https://twitter.com/DummiesLab" rel="noopener noreferrer" className={styles.shiny}></a>
+        <span style={{animationDelay: "-.3s"}}></span>
+        <span style={{animationDelay: "-.6s"}}></span>
+        <span style={{animationDelay: "-.9s"}}></span>
+        <span style={{animationDelay: "-1.2s"}}></span>
     </div>
   );
 };
