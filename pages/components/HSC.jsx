e7dbf5bdcab2bed04bc7a3c23d783660743fbe88
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-21 15:01:33 +0100

    Update HSC.jsx

--- pages/components/HSC.jsx
+++ pages/components/HSC.jsx
@@ -40,11 +40,11 @@ const HSC = () => {
     let windowWidth = window.innerWidth;
     let scrollToPosition;
     if (windowWidth < windowHeightCheck) {
-      docWidth = windowHeight * 6.5;
+      docWidth = windowHeight * 6.3;
       if (isFirefox()) {
         scrollToPosition = (docWidth / 2 - windowWidth / 2) - 250;
       } else if (isSafari()) {
-        scrollToPosition = (docWidth / 2 - windowWidth / 2) + 630;
+        scrollToPosition = (docWidth / 2 - windowWidth / 2) + 930;
       } else if (isChrome()) {
         scrollToPosition = (docWidth / 2 - windowWidth / 2) + 350;
       } else if (isDiscordInAppBrowser()) {
