789a5ddb47c2804d92f3150ee5ed5dd1804033a6
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-21 20:52:43 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Home = () => {
     	  <meta property="og:type" content='website' />
     	  <meta property="og:description" content='Bout to go DUMMY' />
     	  <meta property="og:image" content="/dummiestarp.png" />
-    	  <meta property="og:url" content='https://www.dummies-lab.com/' />
+    	  <meta property="og:url" content='https://dummies-6yv4.vercel.app/' />
        <meta name="og:title" content="Dummies Lab" />
 
         <link rel="apple-touch-icon" sizes="180x180" href="/dummiestarp.png" />
