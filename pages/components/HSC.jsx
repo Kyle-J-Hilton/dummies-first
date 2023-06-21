070fdbee9b9376372a68d55678ac513fa8bb5a40
Kyle-J-Hilton <106459128+Kyle-J-Hilton@users.noreply.github.com>
2023-06-21 11:50:04 +0100

    Update HSC.jsx

--- pages/components/HSC.jsx
+++ pages/components/HSC.jsx
@@ -15,7 +15,7 @@ const HSC = () => {
     let windowHeight = window.innerHeight;
     let windowWidth = window.innerWidth;
     if (windowWidth < windowHeightCheck) {
-      docWidth = windowHeight * 9;
+      docWidth = windowHeight * 9.2;
      
     } else {
       docWidth = window.innerHeight * 9.26;
